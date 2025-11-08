c6d5b0f6caa2f0bce72f9b35c6dd159314bf4693
RamziAymann <zongoramzi@gmail.com>
2025-11-08 17:31:07 +0000

    MAJ de astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site:'https://github.com/RamziAymann/yam.git',
-    base: 'yam',
     output: 'static'
 });
